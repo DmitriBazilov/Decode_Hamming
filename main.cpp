5e4799894b33af33c7bab730f34b18e8962e964d
DmitriBazilov <90473311+DmitriBazilov@users.noreply.github.com>
2021-10-19 20:06:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,14 +35,11 @@ bool check_string(string& s) {
 
 void wrong_par_bit(int bit_index, vector<int>& par_arr) {
     cout << bit_index << " parity bit is wrong\n";
-    cout << bit_index << " parity bit is equal to " << par_arr[bit_index] << endl;
-    cout << "this bit must be equal " << !par_arr[bit_index] << endl;
 }
 
 void wrong_inf_bit(int bit_index, vector<int>& inf_arr) {
     cout << bit_index << " information bit is wrong\n";
-    cout << bit_index << " information bit is equal to " << inf_arr[bit_index] << endl;
-    cout << "this bit must be equal " << !inf_arr[bit_index] << endl;
+    inf_arr[bit_index - 1] = !inf_arr[bit_index - 1];
 }
 
 int main()
@@ -81,16 +78,14 @@ int main()
     int syndrome1 = par_bits[0] ^ inf_bits[0] ^ inf_bits[1] ^ inf_bits[3];
     int syndrome2 = par_bits[1] ^ inf_bits[0] ^ inf_bits[2] ^ inf_bits[3];
     int syndrome3 = par_bits[2] ^ inf_bits[1] ^ inf_bits[2] ^ inf_bits[3];
-    
+
     int sum = syndrome1 + (syndrome2 * 2) + (syndrome3 * 4);
-    
+
     // Определение ошибки
 
     if (sum == 0) {
         cout << "No problem\n";
-        return 0;
     } else {
-        cout << sum << " digit is wrong\n";
         double check = log2(sum);
         if (check == ceil(check)) {
             wrong_par_bit(check + 1, par_bits);
@@ -102,14 +97,13 @@ int main()
                 index = sum - 3;
             wrong_inf_bit(index, inf_bits);
         }
-        message[sum - 1] = !message[sum - 1];
     }
-    
+
     //Вывод правильного сообщения
 
     cout << "right message is ";
-    for (int i = 0; i < message.size(); i++) {
-        cout << message[i];
+    for (int i = 0; i < inf_bits.size(); i++) {
+        cout << inf_bits[i];
     }
     cout << endl;
 
